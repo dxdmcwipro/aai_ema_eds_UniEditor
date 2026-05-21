790a5515621956aa9a1b9c2ebd41b29dfa8b58e4
AEMY Studio <noreply@aemy.studio>
2026-05-21 12:24:17 +0000

    Add cards-product block
    Automated migration updates generated by Experience Modernization Agent.
    
    Changed files (2):
    - blocks/cards-product/cards-product.css
    - blocks/cards-product/cards-product.js

--- blocks/cards-product/cards-product.js
+++ blocks/cards-product/cards-product.js
@@ -2,19 +2,27 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-product-card-image';
-      else div.className = 'cards-product-card-body';
+    [...li.children].forEach((div, index) => {
+      if (index === 0) {
+        div.className = 'cards-product-card-image';
+        const buttonLink = div.querySelector('a.button');
+        if (buttonLink) {
+          buttonLink.classList.remove('button');
+          const container = buttonLink.closest('.button-container');
+          if (container) container.classList.remove('button-container');
+        }
+      } else {
+        div.className = 'cards-product-card-body';
+      }
     });
     ul.append(li);
   });
-  ul.querySelectorAll('picture > img').forEach((img) => {
+  ul.querySelectorAll('.cards-product-card-image picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     moveInstrumentation(img, optimizedPic.querySelector('img'));
     img.closest('picture').replaceWith(optimizedPic);
